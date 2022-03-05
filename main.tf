34c5c3fcb28d7eba07103bacdf8ef3a76060b856
Rene Montilva <renemontilva@gmail.com>
2022-03-05 21:27:48 +0100

    Add spacelift module

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "baseline" {
-  source      = "app.terraform.io/rene_shiphero/baseline/aws"
+  #source      = "app.terraform.io/rene_shiphero/baseline/aws"
+  source      = "spacelift.io/renemontilva/terraform-aws-baseline/default"
   version     = "1.0.1"
   environment = var.environment
 }
