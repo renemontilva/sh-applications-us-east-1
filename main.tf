67844e9041c83f9b5774d7051eba82b0b822a238
Rene Montilva <renemontilva@gmail.com>
2022-03-05 19:10:21 +0100

    Add initial settings

---
+++ main.tf
@@ -0,0 +1,30 @@
+module "baseline" {
+  source  = "rene_shiphero/baseline/aws"
+  version = "1.0.0"
+}
+
+#module "database" {
+#  source  = "shiphero/database/aws"
+#  version = "1.0.1"
+#}
+#
+#module "appendpoint" {
+#  source  = "shiphero/appendpoint/aws"
+#  version = "1.0.0"
+#}
+#
+#module "publicapi" {
+#  source  = "shiphero/publicapi/aws"
+#  version = "1.0.0"
+#}
+#
+#module "eventapp" {
+#  source  = "shiphero/eventapp/aws"
+#  version = "1.0.0"
+#}
+#
+#module "trackingapp" {
+#  source  = "shiphero/trackingapp"
+#  version = "1.0.0"
+#}
+#
