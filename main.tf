0a40a0a24b296b4c43e3d5e0322c893179069161
Rene Montilva <renemontilva@gmail.com>
2022-03-11 16:17:57 +0100

    Add module version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "baseline" {
   source  = "app.terraform.io/rene_shiphero/baseline/aws"
-  version = "1.0.2"
+  version = "1.0.3"
   #source = "spacelift.io/renemontilva/terraform-aws-baseline/default"
   #version     = "0.1.0"
   environment = var.environment
@@ -8,9 +8,9 @@ module "baseline" {
 
 module "eventapp" {
   source             = "app.terraform.io/rene_shiphero/eventapp/aws"
-  version            = "0.0.2"
+  version            = "0.0.5"
   instance_type      = var.instance_type
-  subnet_id          = module.baseline.private_subnet.id
+  subnet_id          = module.baseline.private_subnet.0.id
   security_group_ids = [module.baseline.sg_backend_app.id]
 }
 
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "environment" {
   type        = string
   description = "environment name"
 }
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type."
+}
